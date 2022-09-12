caaad1e05eb4897f94e35d6471461d71490919a5
Tarek <tarek.shehabeldin.ibrahim@gmail.com>
2022-09-12 16:37:19 +0200

    Add API file with needed functions

---
+++ src/API.js
@@ -0,0 +1,27 @@
+// import fetch from "node-fetch"
+
+const api = 'https://plotter-task.herokuapp.com'
+
+const header = {
+    'Accept': 'application/json',
+}
+
+//Get all columns available
+export const getAll = () => {
+    fetch(`${api}/columns`)
+        .then(res =>  res.json())
+        .then(data => data.columns)
+}
+
+//Retrieve data about the dimension & measure(s)
+export const getData = data => {
+    fetch(`${api}/data`, {
+        method: 'POST',
+        headers: {
+          ...header,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data }) })
+        .then(res => res.json())
+        .then(data => data.data)
+}
