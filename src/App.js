40d7180327eb4ff5432ed2676d6849ae7bd75ab7
Tarek <tarek.shehabeldin.ibrahim@gmail.com>
2022-09-15 16:53:22 +0200

    Formatting: > ColumnsList, and Plotter converted into function components. > States moved to App.js and needed data passed as props. > App converted to class component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import React from 'react'
-import './App.css'
+import React, { Component } from 'react'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from "react-dnd-html5-backend"
+// import './App.css'
 import ColumnsList from './Components/ColumnsList.js'
 import Plotter from './Components/Plotter.js'
 import Picker from './Components/Picker.js'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from "react-dnd-html5-backend";
+import * as API from './API.js'
 
-function App() {
-  return (
-    <DndProvider backend={HTML5Backend}>
-      <div className='row-flex'>
-        <ColumnsList />
-        <div className='col2 col-flex'>
-          <Picker />
-          <Plotter />
+class App extends Component {
+
+  state={
+    columns:[],
+    queryResponse: [],
+    plotData: []
+  }
+
+  //Fetch all columns from API & update state with it
+  getColumns = async () => {
+    await API.getAll()
+      .then(res => {
+        // console.log(res)
+        this.setState({columns: res})
+      })
+  }
+
+  // Fetch Query response from API & update state with it
+  getQueryData = async (dimension, measures) => {
+    const data = 
+    {
+      "dimension": dimension,
+      "measures": measures
+    }
+    await API.getData(data)
+      .then(res => {
+        console.log("Retrieved data: ", res)
+        this.setState({queryResponse: res})
+      })
+  }
+
+  buildPlotData = () => {
+    let data = this.state.queryResponse
+    let plotData
+
+    if(data !== []){
+      // Write name of each value of the dimension
+      plotData = data[0].values.map(val => {
+        return {
+          name: val
+        }
+      })
+
+      // Write the values of each measure
+      //remove first element (dimension) to get an array of measures
+      data.shift()
+      for(let i=0 ; i<data.length ; i++){
+        const measure = data[i].name
+        for(let j=0 ; j<plotData.length ; j++){
+          plotData[j][measure] = data[i].values[j]
+        }
+      }
+    }
+    else{
+      return []
+    }
+    
+    console.log("Plot data: ", plotData)
+    return plotData
+  }
+
+  componentDidMount = () => {
+    this.getColumns()
+    this.getQueryData("Product", ["Cost", "Revenue"])
+    // console.log(this.buildPlotData())
+  }
+
+  render() {
+    return (
+      <DndProvider backend={HTML5Backend}>
+        <div className='row-flex'>
+          <ColumnsList columns={this.state.columns} />
+          <div className='col2 col-flex'>
+            <Picker />
+            <Plotter 
+              buildPlotData={this.buildPlotData}
+            />
+          </div>
         </div>
-      </div>
-    </DndProvider>
-  );
+      </DndProvider>
+    )
+  }
+  
 }
 
 export default App
--- src/Components/ColumnsList.js
+++ src/Components/ColumnsList.js
@@ -1,59 +1,37 @@
-import React, { Component } from "react"
-import * as API from '../API.js'
+import React from "react"
 import DraggableButton from "./dragndrop/DraggableButton.js"
 
-class ColumnsList extends Component {
+function ColumnsList ({columns}) {
 
-    state={
-        columns: []
-    }
+    return (
+        <div className="col1">
+                
+            <h2>- Columns -</h2>
+            <ul>
+                <h3>Dimensions:</h3>
+                {
+                    columns.filter(c => c.function === 'dimension').map(col => {
+                        return(
+                            <li key={col.name} >
+                                <DraggableButton id={col.name} name={col.name} type="dimension-button" />
+                            </li>
+                        )
+                    })
+                }
 
-    getColumns = async () => {
-        await API.getAll()
-            .then(res => {
-                // console.log(res)
-                this.setState({columns: res})
-            })
-    }
-
-    componentDidMount = () => {
-        this.getColumns()
-        // this.getData("Product", ["Cost"])
-    }
-
-    render(){
-        return (
-            <div className="col1">
-                    
-                <h2>- Columns -</h2>
-                <ul>
-                    <h3>Dimensions:</h3>
-                    {
-                        this.state.columns.filter(c => c.function === 'dimension').map(col => {
-                            return(
-                                <li key={col.name} >
-                                    {/* <button > {col.name} </button> */}
-                                    <DraggableButton id={col.name} name={col.name} type="dimension-button" />
-                                </li>
-                            )
-                        })
-                    }
-
-                    <h3>Measures:</h3>
-                    {
-                        this.state.columns.filter(c => c.function === 'measure').map(col => {
-                            return(
-                                <li key={col.name} >
-                                    {/* <button > {col.name} </button> */}
-                                    <DraggableButton id={col.name} name={col.name} type="measure-button" />
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </div>
-        )
-    }
+                <h3>Measures:</h3>
+                {
+                    columns.filter(c => c.function === 'measure').map(col => {
+                        return(
+                            <li key={col.name} >
+                                <DraggableButton id={col.name} name={col.name} type="measure-button" />
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default ColumnsList
